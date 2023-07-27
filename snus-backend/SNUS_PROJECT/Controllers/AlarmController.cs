100943170cc73d7a10a0b8708ea9004cd8db5882
Anja Petković <anjapetkovic92@gmail.com>
2023-07-28 01:57:02 +0200

    Final changes

--- snus-backend/SNUS_PROJECT/Controllers/AlarmController.cs
+++ snus-backend/SNUS_PROJECT/Controllers/AlarmController.cs
@@ -10,10 +10,12 @@ namespace SNUS_PROJECT.Controllers
     public class AlarmController : Controller
     {
         private readonly IAlarmRepository _alarmRepository;
+        private readonly IAnalogInputRepository _analogInputRepository;
 
-        public AlarmController(IAlarmRepository alarmRepository)
+        public AlarmController(IAlarmRepository alarmRepository, IAnalogInputRepository analogInputRepository)
         {
-            _alarmRepository = alarmRepository; 
+            _alarmRepository = alarmRepository;
+            _analogInputRepository = analogInputRepository;
         }
 
         [HttpGet("all")]
@@ -34,7 +36,9 @@ namespace SNUS_PROJECT.Controllers
             try
             {
                 Alarm alarm1 = new Alarm(alarm);
-                //TODO : setovati ostale parametre
+                AnalogInput ai = _analogInputRepository.GetAnalogInput((int)alarm.AnalogId);
+                alarm1.AnalogInput = ai;
+                alarm1.MeasureUnit = ai.Units;
                 _alarmRepository.AddAlarm(alarm1);
                 return Ok();
             }
